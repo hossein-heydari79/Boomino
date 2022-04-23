f282bb139f52efbdcc4055d50275f224d85a1de0
hossein-heydari <hosseinheydari790228@gmail.com>
2022-04-23 20:33:44 +0430

    add remove select rank

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,6 +3,9 @@ import dislike from "assets/icons/dislike.svg";
 import like from "assets/icons/like.svg";
 import close from "assets/icons/close.svg";
 
+import likeSelected from "assets/icons/like-select.svg";
+import disLikeSelected from "assets/icons/dislike-select.svg";
+
 
 const Modal = ({ movieSelected, setMovieSelected }) => {
 
@@ -21,6 +24,15 @@ const Modal = ({ movieSelected, setMovieSelected }) => {
         })
     }
 
+
+
+    const removeRankSelect = () => {
+        setMovieSelected({
+            ...movieSelected,
+            rankSelected: ""
+        })
+    }
+
     return (
         <div className={styles.modal}>
             <div className={styles.box}>
@@ -36,11 +48,24 @@ const Modal = ({ movieSelected, setMovieSelected }) => {
                 </h2>
                 <div className={styles.imageBox}>
                     <div className={styles.likeBox}>
-                        <img src={like} onClick={() => rankSelectedFunc("much")} alt="likeLogo" />
+                        {
+                            movieSelected.rankSelected === "much"
+                                ?
+                                <img src={likeSelected} onClick={removeRankSelect} alt="likeLogo" />
+                                :
+                                <img src={like} onClick={() => rankSelectedFunc("much")} alt="likeLogo" />
+                        }
                         <h2 style={{ color: movieSelected.rankSelected == "much" ? "rgb(0, 230, 0)" : "#FFFFFF" }} className={styles.much}>زیاد</h2>
                     </div>
                     <div className={styles.dislikeBox}>
-                        <img src={dislike} onClick={() => rankSelectedFunc("low")} alt="dislikeLogo" />
+
+                        {
+                            movieSelected.rankSelected === "low"
+                                ?
+                                <img src={disLikeSelected} onClick={removeRankSelect} alt="likeLogo" />
+                                :
+                                <img src={dislike} onClick={() => rankSelectedFunc("low")} alt="dislikeLogo" />
+                        }
                         <h2 style={{ color: movieSelected.rankSelected == "low" ? "rgb(255, 58, 58)" : "#FFFFFF" }} className={styles.low}>کم</h2>
                     </div>
                 </div>
